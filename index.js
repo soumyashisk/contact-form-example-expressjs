e6a10f557652e6a0eec3e8691e2d5e62fc8c9440
Soumyashis Karmakar <soumyashisk@gmail.com>
2025-10-26 19:36:41 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+const app = express();
+
+// middlewares
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const messages = [];
+
+// routes
+app.post("/contact", (req, res) => {
+  const emailRegex = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/gm;
+  if (emailRegex.test(req.body.email) && req.body.message) {
+    messages.push(req.body);
+    return res.redirect("/thankyou.html");
+  }
+  return res.redirect("/invalid.html");
+});
+
+app.get("/messages", (_, res) => {
+  res.json(messages);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
