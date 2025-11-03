05f4529754144f16fa2a9de3cfca4607bce4cbf1
Soumyashis Karmakar <soumyashisk@gmail.com>
2025-11-03 19:04:37 +0530

    Refactor database handling and enhance message retrieval functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import Database from "better-sqlite3";
 
 const app = express();
 
@@ -13,24 +14,43 @@ app.use(
   })
 );
 
-const messages = [];
+const db = new Database("database.db");
+db.pragma("journal_mode = WAL");
+
+db.exec(`
+CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT NOT NULL,
+    message TEXT NOT NULL
+);
+`);
+const insert = db.prepare(
+  "INSERT INTO messages (email, message) VALUES (?, ?)"
+);
+const selectAll = db.prepare("SELECT * FROM messages ORDER BY id ASC");
 
 // routes
 app.post("/contact", (req, res) => {
   const emailRegex = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/gm;
   if (emailRegex.test(req.body.email) && req.body.message) {
-    messages.push(req.body);
+    insert.run(req.body.email, req.body.message);
     return res.redirect("/thankyou.html");
   }
   return res.redirect("/invalid.html");
 });
 
-app.get("/messages", (_, res) => res.json(messages));
+app.get("/messages", (_, res) => {
+  const messages = selectAll.all();
+  res.json(messages);
+});
 
 app.get("/messages/:index", (req, res) => {
   const i = req.params.index;
-  let response = { data: null, error: null };
-  if (messages.length === 0) {
+  const messagesLength = db
+    .prepare("SELECT COUNT(*) AS count FROM messages")
+    .get().count;
+  const response = { data: null, error: null };
+  if (messagesLength === 0) {
     response.error = "There are no messages";
   } else if (isNaN(Number(i))) {
     response.error = "Index is not a number";
@@ -38,11 +58,8 @@ app.get("/messages/:index", (req, res) => {
     response.error = "Index must be an integer";
   } else if (!(i >= 0)) {
     response.error = "Index must be a positive or zero";
-  } else if (!(i < messages.length)) {
-    response.error = "Index is greater than the number of messages";
   } else {
-    response.data = messages[i];
-    messages.splice(i, 1);
+    response.data = db.prepare("SELECT * FROM messages WHERE id = ?").get(i);
   }
   return res.json(response);
 });
